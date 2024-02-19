1eb55d83ab571f3dbcfe68ff5925b0fcf2437e2e
kchvbf <109752188+kchvbf@users.noreply.github.com>
2024-02-19 13:46:48 +0300

    Add files via upload

---
+++ playground.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+let Surname1 = "Качаев"
+let Name = "Борис"
+let Surname2 = "Фаридович"
+let age = 21
+let height = 181.1
+let isJob = true
+
+print("Фамилия: \(Surname1) \n Имя: \(Name) \n Отчество: \(Surname2) \n Возраст: \(age) \n Рост: \(height) \n Трудоустроен?: \(isJob)")
